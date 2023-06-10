12c13eadc8213958a226318872bf87621326dd14
itisMilan <abhaymilan8@gmail.com>
2023-06-10 17:12:09 +0530

    Task Tracker

---
+++ src/components/Task.vue
@@ -0,0 +1,50 @@
+<template>
+  <div @dblclick="$emit('toggle-reminder',task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <div class="task2">
+
+        <h3>{{ task.text }}</h3>
+        <i @click="onDelete(task.id)" class="fa-solid fa-xmark"></i>
+    </div>
+    <p>{{ task.day }}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+  methods:{
+    onDelete(id){
+       this.$emit('delete-task',id)
+    }
+  }
+};
+</script>
+<style scope>
+.fa-xmark{
+    color:red;
+}
+.task {
+    background: #f4f4f4;
+    /* color: black; */
+    margin:5px;
+    padding:10px 20px;
+    cursor:pointer;
+}
+.task.reminder{
+    border-left: 5px solid green;
+}
+.task2{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+/* .task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+} */
+</style>
---
+++ vue-project/src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    
+    <button @click="onclick()"
+    
+    v-bind:style="{background:color}" class="btn">{{ text }}</button>
+</template>
+
+
+<script>
+export default{
+    name:"Button",
+    props:{
+        text:String,
+        color:String
+    },
+    methods:{
+        onclick(){
+          this.$emit('btn-click')
+        }
+    }
+}</script>
